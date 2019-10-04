f11324bbc6dd8447095dfd9b3785f77aa5b21cf1
Landon Young <landon.young@canopytax.com>
2019-10-03 22:43:58 -0600

    started runner

--- run.js
+++ run.js
@@ -1,3 +1,31 @@
-import prompt
-let running = true
-console.log('is run')
+var prompt = require('prompt')
+var classes = require('./elevator.js')
+
+var promptAttrs = [
+    {
+        name: 'elevators',
+        validator: /\d+/,
+        warning: 'must be number'
+    },
+    {
+        name: 'floors'
+    }
+]
+prompt.start()
+
+
+console.log('How many elevators?')
+prompt.get(promptAttrs, function (err, result) {
+        // Get user input from result object.
+        var elevators = result.username;
+});
+console.log('How many floors?')
+prompt.get(promptAttrs, function (err, result) {
+        // Get user input from result object.
+        var floors = result.username;
+});
+
+
+var simulation = new classes.Simulation(elevators, floors)
+
+console.log(simulation.getState())
