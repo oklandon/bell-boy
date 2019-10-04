dac4d89326b594ea4e5d590b12cf681d4160567a
Landon Young <landon.young@canopytax.com>
2019-10-03 21:45:30 -0600

    test basic elevator and simulator classes

--- elevator.js
+++ elevator.js
@@ -1,34 +1,43 @@
 export function elevatorController(state, action={}){
     switch(action.opName){
         default:
-            return controllerState
+            return state
     }
 }
 
-export default function simulate(numElevators, totalFloors){
-    const state =
-}
+// export default function simulate(numElevators, totalFloors){
+//     const state =
+// }
 
 
 export class Simulator {
     constructor(numElevators, totalFloors) {
             let elevators = []
             for (let i = 0; i < numElevators; i++) {
+                elevators.push(
+                    new Elevator(totalFloors, `num_${i+1}`)
+                )
             }
             this.state = elevatorController({
-                elevators=
+                elevators,
+                totalFloors
             })
     }
 
     summonElevator(floorNumber){
+        return true
+    }
+
+    getState(){
+        return this.state
     }
 }
 
 
 export class Elevator {
-    constructor(topFloor, name) {
+    constructor(topFloor, name, currentFloor=1) {
         this.name= name
-        this.currentFloor = 1
+        this.currentFloor = currentFloor
         this.targetFloor = null
         this.door_open = false
         this.total_trips = 0
@@ -36,5 +45,19 @@ export class Elevator {
         this.floors_passed = 0
         this.active = true
         this.topFloor = topFloor
+    }
+
+    moveOne(targetFloor){
+        this.currentFloor = targetFloor >= this.currentFloor
+            ? this.currentFloor += 1
+            : this.currentFloor -= 1
+    }
+
+    where(){
+        return this.currentFloor
+    }
+
+    declareDistance(targetFloor){
+        return Math.abs(targetFloor - this.currentFloor)
     }
 }
--- project.test.js
+++ project.test.js
@@ -1,3 +1,33 @@
-test('first', () => {
-	expect(2).toEqual(2)
+import { Simulator, Elevator } from './elevator.js'
+
+test('initalizes with right amount of elevators', () => {
+    // try with one elevator
+    let sim = new Simulator(1, 2)
+    // try with 1000 elevators
+    let sim2 = new Simulator(1000, 2)
+    expect(sim.getState().elevators.length).toEqual(1)
+    expect(sim2.getState().elevators.length).toEqual(1000)
+})
+
+test('move elevator', () => {
+    let onFloorOne = new Elevator(10, 'Craigary', 1)
+
+    // goes up
+    onFloorOne.moveOne(5)
+    expect(onFloorOne.where()).toEqual(2)
+    // goes down
+    onFloorOne.moveOne(1)
+    expect(onFloorOne.where()).toEqual(1)
+})
+
+test('find where elevator is', () => {
+    let onFloorThree = new Elevator(10, 'jimothy', 3)
+
+    let moves = 0
+    const targetFloor = 10
+    while(moves < 3){
+        onFloorThree.moveOne(targetFloor)
+        moves++
+    }
+    expect(onFloorThree.where()).toEqual(6)
 })
